9cecb096d125b97a923df8c41bb19e50358b8965
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-06-15 14:52:48 +0300

    Convert add new plain text form view to allow edit existing texts

--- bibliothecula-django/bibliothecula/templatetags/help_tags.py
+++ bibliothecula-django/bibliothecula/templatetags/help_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
+from ..forms import TextStorageEdit
+from ..models import THUMBNAIL_MIMES
 
 register = template.Library()
 
@@ -17,3 +19,13 @@ def return_item(l, i):
 def section_url(htmlid):
     ret = f"""<a id="header-anchor-{htmlid}" class="header-anchor" aria-hidden="true" href="#{htmlid}"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><use href="#header-link-svg"></use></svg></a>"""
     return mark_safe(ret)
+
+
+@register.filter(is_safe=False)
+def is_plain_text(content_type):
+    return content_type in TextStorageEdit.datalist_options
+
+
+@register.filter(is_safe=False)
+def is_thumbnail_mime(content_type):
+    return content_type in THUMBNAIL_MIMES
