dcb5dee4ce429d717e96cbe833092b785270be0d
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-06-20 01:12:20 +0300

    bumblebat: add reflinks in note view

--- bumblebat/bibliothecula/views/storage.py
+++ bumblebat/bibliothecula/views/storage.py
@@ -3,15 +3,145 @@ from django import db
 from django.http import FileResponse
 from django.views.decorators.http import condition
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 from uuid import UUID
 import html
+
+from markdown_it.rules_inline import StateInline
+from markdown_it.token import Token
+
+
+def reflink(md):
+    def reflink_render(self, tokens, idx, options, env):
+        dbg(self)
+        dbg(idx)
+        dbg([x for x in enumerate(tokens)])
+        tok = tokens[idx]
+        dbg(tok)
+        return self.renderToken(tokens, idx, options, env)
+
+    def reflink_def(state: StateInline, silent: bool) -> bool:
+        pos = state.pos
+
+        if state.srcCharCode[pos] != 0x3C:  # /* < */
+            return False
+
+        start = state.pos
+        maximum = state.posMax
+
+        while True:
+            pos += 1
+            if pos >= maximum:
+                return False
+
+            ch = state.srcCharCode[pos]
+
+            if ch == 0x3C:  # /* < */
+                return False
+            if ch == 0x3E:  # /* > */
+                break
+
+        url = state.src[start + 1 : pos]
+        print("url is ", url)
+        try:
+            uuid = UUID(url)
+        except ValueError:
+            print("valuerror", url)
+            return False
+        print("uuid success ", uuid)
+
+        target = None
+        try:
+            _doc = Document.objects.all().get(binary_metadata__metadata__uuid=uuid)
+            met = BinaryMetadata.objects.all().get(uuid=uuid)
+            target = (_doc, met)
+        except Document.DoesNotExist:
+            pass
+        except BinaryMetadata.DoesNotExist:
+            pass
+        print("first get result", target)
+        if target is None:
+            try:
+                _doc = Document.objects.all().get(uuid=uuid)
+                target = (_doc, None)
+            except Document.DoesNotExist:
+                pass
+            except BinaryMetadata.DoesNotExist:
+                pass
+        print("sec get result", target)
+
+        if target is None:
+            return False
+
+        target_url = reverse(
+                    "document_storage_viewer", args=[target[0].uuid, target[1].uuid]
+                )
+        if not silent:
+            token = state.push("link_open", "a", 1)
+            token.attrs = {}
+            if target[1] is None:
+                token.attrs["href"] = target[0].get_absolute_url()
+                token.attrs["id"] = f"ref-{target[0].uuid}"
+            else:
+                token.attrs["href"] = target_url
+                token.attrs["id"] = f"ref-{target[1].uuid}"
+            token.attrs["class"] = "ref"
+            token.markup = "reflink"
+            token.info = "auto"
+            token.meta["id"] = reflink_def.refcount
+            token.meta["ref"] = {}
+
+            token = state.push("text", "", 0)
+            if target[1] is None:
+                token.content = str(target[0].uuid)
+            else:
+                token.content = str(target[1].uuid)
+
+            token = state.push("link_close", "a", -1)
+            token.markup = "reflink"
+            token.info = "auto"
+            token = state.push("sup_open", "sup", 1)
+            token = state.push("text", "", 0)
+            token = state.push("sup_close", "sup", -1)
+            token = state.push("span_open", "span", 1)
+            token.attrs["class"] = "ref-expand"
+            token.attrs["data-ref-num"] = reflink_def.refcount
+            if target[1] is None:
+                token.attrs["id"] = f"ref-expand-{target[1].uuid}"
+                token = state.push("link_open", "a", 1)
+                token.attrs["href"] = target_url
+                token = state.push("text", "", 0)
+                _type = target[0].try_get_content_type()
+                token.content = _type["filename"] if _type else str(target[0])
+                token = state.push("link_close", "a", -1)
+            else:
+                token.attrs["id"] = f"ref-expand-{target[1].uuid}"
+                token = state.push("link_open", "a", 1)
+                token.attrs["href"] = target_url
+                token = state.push("text", "", 0)
+                _type = target[1].try_get_content_type()
+                token.content = _type["filename"] if _type else str(target[1])
+                token = state.push("link_close", "a", -1)
+            token = state.push("span_close", "span", -1)
+
+            reflink_def.refcount += 1
+
+        dbg(dir(state))
+        state.pos += len(url) + 2
+        return True
+
+    reflink_def.refcount = 1
+    md.inline.ruler.after("autolink", "reflink_def", reflink_def)
+    md.add_render_rule("link_open", reflink_render, fmt="html")
 
 
 @staff_member_required
 @condition(last_modified_func=last_modified_binary_metadata)
 def view_document_storage(request, uuid, metadata_uuid):
-    uuid = UUID(uuid)
-    metadata_uuid = UUID(metadata_uuid)
+    if not isinstance(uuid, UUID):
+        uuid = UUID(uuid)
+    if not isinstance(metadata_uuid, UUID):
+        metadata_uuid = UUID(metadata_uuid)
 
     try:
         m = BinaryMetadata.objects.get(pk=metadata_uuid)
@@ -52,7 +182,7 @@ def text2html(text, content_type):
         try:
             from markdown_it import MarkdownIt
 
-            md = MarkdownIt()
+            md = MarkdownIt().use(reflink)
             return mark_safe(md.render(text))
         except ModuleNotFoundError:
             pass
@@ -99,12 +229,17 @@ def document_storage_viewer(request, uuid, metadata_uuid):
     filename = _t["filename"]
 
     refs = None
+    backrefs = None
     try:
         with connections["bibliothecula"].cursor() as cursor:
             cursor.execute(
-                f"SELECT target FROM backrefs_fts WHERE referrer = '{doc.uuid.hex}'"
+                f"SELECT target FROM backrefs_fts WHERE referrer = '{m.uuid.hex}'"
             )
             refs = cursor.fetchall()
+            cursor.execute(
+                f"SELECT referrer FROM backrefs_fts WHERE target = '{m.uuid.hex}'"
+            )
+            backrefs = cursor.fetchall()
         print("refs: ", refs)
     except db.OperationalError:
         pass
@@ -116,10 +251,22 @@ def document_storage_viewer(request, uuid, metadata_uuid):
                     binary_metadata__metadata__uuid=ref[0]
                 )
                 met = BinaryMetadata.objects.all().get(uuid=ref[0])
-                objs.append((_doc, met))
+                objs.append((_doc, met, met.try_get_content_type()))
             except Document.DoesNotExist:
                 continue
         refs = objs
+    if backrefs is not None:
+        objs = []
+        for backref in backrefs:
+            try:
+                _doc = Document.objects.all().get(
+                    binary_metadata__metadata__uuid=backref[0]
+                )
+                met = BinaryMetadata.objects.all().get(uuid=backref[0])
+                objs.append((_doc, met, met.try_get_content_type()))
+            except Document.DoesNotExist:
+                continue
+        backrefs = objs
 
     context = {
         "title": doc.title,
@@ -129,6 +276,7 @@ def document_storage_viewer(request, uuid, metadata_uuid):
         "text_raw": text_raw,
         "type_": _t,
         "refs": None if refs == [] else refs,
+        "backrefs": None if backrefs == [] else backrefs,
         "add_document_form": AddDocument(),
     }
     template = loader.get_template("text_viewer.html")
